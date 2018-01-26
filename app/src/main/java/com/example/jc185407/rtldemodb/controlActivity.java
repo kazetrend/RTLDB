a6729aaaddcf4b780cd48b2e2b697d429ac84ef6
Jireh Cordova <kazetrend@gmail.com>
2018-01-26 09:27:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jc185407/rtldemodb/controlActivity.java
@@ -0,0 +1,19 @@
+package com.example.jc185407.rtldemodb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jc185407 on 1/26/18.
+ */
+
+public class controlActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
